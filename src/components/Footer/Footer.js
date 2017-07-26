11f07289fca8f1895e7af0009650933125a48115
Supremo Samuel Diogo <samudiogo@gmail.com>
2017-07-26 00:58:53 -0300

    finish compare-react

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,28 @@
 import React from 'react';
-
+import './Footer.css';
 const Footer = () => {
     
     return (
-        <div className="Footer">
-        Hi from footer
+        <div>
+            <footer className="footer is-primary">
+
+                <div className="container">
+                    <div className="columns">
+                        <div className="column">
+                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                        </div>
+                        <div className="column has-text-right">
+                            <a href="#" className="icon">
+                                <i className="fa fa-facebook"></i>
+                            </a>
+                            <a href="#" className="icon">
+                                <i className="fa fa-twitter"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </footer>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
-
+import './Home.css';
 class Home extends Component {
     render() {
+
+        let heading = "Soaring to the new heights";
+        let subheading ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam posuere nec sem quis congue. Morbi at rutrum enim.";
+
         return (
-            <div className="Home">
-            Hi from Home
+            <div>
+                <section className="hero">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                { heading }
+                            </h1>
+                            <div className="is-two-thirds column is-paddingless">
+                                <h2 className="subtitle">
+                                    { subheading }
+                                </h2>
+                            </div>
+                            <router-link to="learn" className="button is-large is-primary" id="learn">Learn more</router-link>
+                        </div>
+                    </div>
+                </section>
+
+                <section className="section">
+                    <div className="container">
+                        <div className="columns pd is-desktop">
+                            <div className="column is-1 has-text-centered">
+                                <i className="fa fa-5x fa-cog is-primary"></i>
+                            </div>
+                            <div className="column is-one-third-desktop">
+                                <p className="title">
+                                    <strong>We provide superior logistics so that your business can succeed in a crazy world </strong>
+                                </p>
+                            </div>
+                            <div className="column">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam posuere nec sem quis congue. Morbi at rutrum enim</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="columns pd">
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <p className="title">One title Nam posuere nec sem quis congue. </p>
+                                    <p className="subtitle">- Samuel Diogo</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <p className="title">Centered title Nam posuere nec sem quis congue. </p>
+                                    <p className="subtitle">- Samuel Diogo</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <div className="card-content">
+                                    <p className="title">last title Nam posuere nec sem quis congue. </p>
+                                    <p className="subtitle">- Samuel Diogo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         );
     }
